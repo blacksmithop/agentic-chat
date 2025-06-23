1e659c1f11cbc5e2a5bbb51c493b9393bc4d5001
Abhinav KM <abhinav.km@thoughtminds.io>
2025-06-23 16:57:46 +0530

    refactor: Remove refactored schema

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 from .config import settings
 from .llm import chat_model, embedding_model
-from .schema import ConfigSchema, State
+from .schema import State
 from .tool import BasicToolNode, route_tools, tools
 from .graph import graph
 from .retriever import retriever_tool
