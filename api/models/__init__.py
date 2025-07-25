3533065962df4cbcf87d863635de85f6a3f7f8b0
Abhinav KM <abhinav.km@thoughtminds.io>
2025-07-25 09:56:29 +0530

    chore: Remove legacy api logic

--- api/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .user import User
-from .message import Message, MessageType
-from .chat import PrivateChat
-from .moderation import ModerationLog
-
-__all__ = ["User", "Message", "MessageType", "PrivateChat", "ModerationLog"]
